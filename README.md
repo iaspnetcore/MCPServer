1f94a3f1c78c2aac376cff24d9317df696806eb0
iaspnetcore <290996724@qq.com>
2025-05-25 23:54:26 +0800

    Define Tools in mcp server

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 F:\developer_mcp
 
 https://github.com/iaspnetcore/MCPServer.git/
+
+blog:https://www.iaspnetcore.com/blogpost-683332645fb02c0677df0289-mcp-server-in-c-net
---
+++ src/McpServerConsole.Server/Tools/TimeTool.cs
@@ -0,0 +1,18 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McpServerConsole.Server.Tools
+{
+    [McpServerToolType]
+    public static class TimeTool
+    {
+        [McpServerTool, Description("Get the current time for a city")]
+        public static string GetCurrentTime(string city) =>
+            $"It is {DateTime.Now.Hour}:{DateTime.Now.Minute} in {city}.";
+    }
+}
