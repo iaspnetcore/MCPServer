33e996ee7c6707871ba7441d3f78cabbe2d117b4
iaspnetcore <290996724@qq.com>
2025-05-26 00:15:05 +0800

    Update Program.cs to set up the MCP server

--- src/McpServerConsole.Server/Program.cs
+++ src/McpServerConsole.Server/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace McpServerConsole.Server
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly(); //WithToolsFromAssembly扩展方法，会自动扫描程序集中添加了McpServerTool标签的类进行注册
+
+await builder.Build().RunAsync();
