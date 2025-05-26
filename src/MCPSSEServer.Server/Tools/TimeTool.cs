bed358b5d1caea4faac653c137bf0c3b2d349b6c
iaspnetcore <290996724@qq.com>
2025-05-26 22:12:16 +0800

    MCPSSEServer: Define Tools

---
+++ src/MCPSSEServer.Server/Tools/TimeTool.cs
@@ -0,0 +1,14 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+
+namespace MCPSSEServer.Server.Tools
+{
+    [McpServerToolType]
+    public static class TimeTool
+    {
+        [McpServerTool, Description("Get the current time for a city")]
+        public static string GetCurrentTime(string city) =>
+            $"It is {DateTime.Now.Hour}:{DateTime.Now.Minute} in {city}.";
+    }
+}
