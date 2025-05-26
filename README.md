c6f81769949c6c64f9efcf95ea55fdc2dcfa63b8
iaspnetcore <290996724@qq.com>
2025-05-27 03:44:19 +0800

    MCPSSEServer.Client：Update your Program.cs to connect the MCP SSE server

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ blog:https://www.iaspnetcore.com/blogpost-683332645fb02c0677df0289-mcp-server-in
 
 The WithToolsFromAssembly will scan the assembly for classes with the McpServerToolType attribute and register all methods with the McpServerTool attribute. Notice that the McpServerTool has a Description which will be fed into any client connecting to the server. This description helps the client determine which tool to call.
 
+## Projects
+
+### MCPServer WithStdioServerTransport
+
+### MCPServer WithHttpTransport(SSE)
 
 [MCPSSEServer.Server](https://github.com/iaspnetcore/MCPServer/tree/master/src/MCPSSEServer.Server) A Simple .NET C# MCP Server using SSE transport using modelcontextprotocol / csharp-sdk
 
--- src/MCPSSEServer.Client/Program.cs
+++ src/MCPSSEServer.Client/Program.cs
@@ -1,22 +1,38 @@
-﻿using ModelContextProtocol.Client;
+﻿
+using ModelContextProtocol.Client; // 包含 McpClientFactory 和 McpClient 相关定义
+
 
 
 
 
 Console.WriteLine("Now connecting client to MCPSSEServer.Server ");
 
-
-
-var clientTransport = new StdioClientTransport(new()
+try
 {
-    Id = "time",
-    Name = "Time MCP Server",
-    TransportType = TransportTypes.Sse,
-    Location = "https://localhost:8443/sse"
-});
+
+    // 创建一个 SSE（Server-Sent Events）客户端传输配置实例
+    var clientTransport = new SseClientTransport(
+
+    // 配置传输选项，指定服务端点（Endpoint）
+    new SseClientTransportOptions()
+    {
+        // 设置远程服务器的 URI 地址  (记得替换真实的地址，从魔搭MCP广场获取)
+        Endpoint = new Uri("https://mcp.api-inference.modelscope.cn/sse/215e9461d2xxxxx")
+    }
+    );
 
 
 
-await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
 
-Console.WriteLine("Successfully connected!");
+    // 使用配置创建 MCP 客户端实例
+    await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+
+
+    Console.WriteLine("Successfully connected!");
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Host terminated unexpectedly : {ex.Message}");
+}
+
