22affc45d61a9bea896a5ff088c14f39ef92abbf
iaspnetcore <290996724@qq.com>
2025-06-09 11:46:55 +0800

    MCPSSEServer.HA.Client: create project for access home assistant mcp server

---
+++ src/MCPSSEServer.HA.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MCPSSEServer.HA.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
